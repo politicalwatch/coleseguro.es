2a3185a8271407a21f2315f132684c0214d29a23
David Hernandez <davidhernandezruiz@gmail.com>
2020-10-28 17:57:05 +0100

    Updates the list of centres to include municipalities.

--- scripts/json-generate.py
+++ scripts/json-generate.py
@@ -5,14 +5,14 @@ import json
 postal_codes = {}
 
 with open('cp.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_reader = csv.reader(csv_file, delimiter='\t')
     is_first_row = True
 
     for row in csv_reader:
         if is_first_row:
             is_first_row = False
             continue
-        key = row[2]
+        key = row[0]
         value = row[1]
 
         postal_codes[key] = value
@@ -31,12 +31,12 @@ with open(sys.argv[1]) as csv_file:
         postal_code = row[8]
         locality = row[4]
         province = row[3]
+        school_name = row[1] + ' (' + row[5] + ')'
 
         if postal_code in postal_codes and locality != postal_codes[postal_code]:
             key = locality + ' / ' + postal_codes[postal_code] + ' (' + province + ')'
         else:
             key = locality + ' (' + province + ')'
-        school_name = row[1] + ' (' + row[5] + ')'
 
         if key not in schools:
             schools[key] = []
